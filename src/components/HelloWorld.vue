ca2dba3f2a96e3537d8c722769e3437b58a380cf
luchengchen <1203018167@qq.com>
2023-09-05 18:07:55 +0800

    ces

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const bigList = new Array(200000).fill(null).map((_ele, i) => ({ n: i + 1 }));
+console.log(bigList, "11");
+</script>
+
+<template>
+  <div class="faDiv">
+    <div class="children">
+      <div class="list">
+        <div class="row" v-for="(item, i) in bigList">
+          {{ item.n }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.faDiv {
+  position: relative;
+  left:0;
+  right:0;
+  top:0;
+  height: 400px;
+  overflow-y: scroll;
+}
+</style>
