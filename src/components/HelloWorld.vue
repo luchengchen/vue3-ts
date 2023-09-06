3cb7d134403260e21aa7397ce573be36897fd3db
luchengchen <1203018167@qq.com>
2023-09-06 15:03:37 +0800

    sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,24 +6,27 @@ console.log(bigList, "11");
 </script>
 
 <template>
-  <div class="faDiv">
-    <div class="children">
+  <div>
+    <div class="container">
       <div class="list">
         <div class="row" v-for="(item, i) in bigList" :key="i">
           {{ item.n }}
         </div>
       </div>
+      <div class="bar"></div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.faDiv {
+<style lang="scss" scoped>
+.container {
   position: relative;
-  left: 0;
-  right: 0;
-  top: 0;
-  height: 400px;
+  width: 500px;
   overflow-y: scroll;
+}
+.list {
+  position: absolute;
+  top:0;
+  width: 100%;
 }
 </style>
