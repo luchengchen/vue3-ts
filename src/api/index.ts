beeffd805c02b14bda3e0036c6ce3d889b2575de
luchengchen <1203018167@qq.com>
2023-10-10 17:38:29 +0800

    feat: api文件

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,9 @@
 import request from "../router/request";
 
-export interface ApiResult<T>{
-    code: number;
-    message: string;
-    data: T
+export interface ApiResult<T> {
+  code: number;
+  message: string;
+  data: T;
 }
 export const userLogin = (params?: any): Promise<ApiResult<T>> =>
   request().get(`/api/opcloud/sys/signIn`, { params: params });
-
-
