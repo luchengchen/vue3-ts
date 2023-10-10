601a7cea464d3f01f33b515faa7ed3cc5e08f249
luchengchen <1203018167@qq.com>
2023-10-10 17:37:55 +0800

    feat: 登录

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,11 @@
 import request from "../router/request";
 
-export const userLogin = (params?: any): Promise<ResponseType> =>
+export interface ApiResult<T>{
+    code: number;
+    message: string;
+    data: T
+}
+export const userLogin = (params?: any): Promise<ApiResult<T>> =>
   request().get(`/api/opcloud/sys/signIn`, { params: params });
+
+
--- src/views/Login/LoginIndex.vue
+++ src/views/Login/LoginIndex.vue
@@ -5,7 +5,7 @@
       <el-main>
         <el-form :model="form" label-width="120px">
           <el-form-item label="账户名">
-            <el-input v-model="form.name" />
+            <el-input v-model="form.username" />
           </el-form-item>
           <el-form-item label="密码">
             <el-input v-model="form.password" type="password" />
@@ -19,21 +19,44 @@
 <script lang="ts" setup>
 import { reactive } from "vue";
 import { userLogin } from "../../api/index";
+import { setStorage, setToken } from "../../utils/utils";
+import { useRouter } from "vue-router";
 const form = reactive({
-  name: "",
-  password: "",
+  orgNumber: 666,
+  username: "luchengchen",
+  password: "1234567_",
 });
+const router = useRouter();
 const login = async () => {
   let para = {
-    username: "luchengchen_666",
-    orgNumber: "6666",
-    password: "1234567_",
+    username: form.username + "_" + form.orgNumber,
+    orgNumber: form.orgNumber,
+    password: form.password,
     grant_type: "password",
     client_id: "opcloud",
     client_secret: "1",
   };
   const res = await userLogin(para);
-  console.log(res, "reeee");
+  if (res.data.code == 200) {
+    goPlace(res.data);
+  }
+};
+//登录跳转
+const goPlace = (res: {
+  data: { expiresIn: number; access_token: string };
+}) => {
+  const { expiresIn, access_token } = res.data;
+  setToken("accessToken", access_token, expiresIn);
+  setStorage("orgNumber", form.orgNumber);
+  setStorage("username", form.username);
+  setToken("userInfo", res.data, expiresIn);
+  const routeQuery = router.currentRoute.value.query;
+  if (routeQuery && routeQuery.redirect) {
+    const redirect: string = routeQuery.redirect as string;
+    router.replace(decodeURIComponent(redirect));
+  } else {
+    router.replace("/");
+  }
 };
 </script>
 <style></style>
